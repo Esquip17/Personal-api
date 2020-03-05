db05723897ea7da638b71afe4f66cf02f79c7b77
Norman Calhoun <ndc1963@gmail.com>
2020-03-05 14:08:03 -0500

    changed index

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
-const fs = require("fs");
-const fetch = require("node-fetch");
-const url = "https://restcountries.eu/rest/v2/all";
+const express = require("express");
+const app = express();
+const parser = require("body-parser");
+const Country = require("./models");
 
-fetch(url)
-  .then(res => res.json())
-  .then(res => {
-    console.log(res);
-    let countries = JSON.stringify(res);
-    fs.writeFile("./db/data.json", countries, err => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("success");
-      }
-    });
+app.use(parser.json());
+
+app.get("/models", function(req, res) {
+  Countrh.find({}).then(Country => {
+    res.json(Country);
   });
+});
+
+app.get("/models/:id", function(req, res) {
+  Country.findOne({ _id: req.params.id }).then(Country => {
+    res.json(list);
+  });
+});
+
+// app.get("/models/:id", function(req, res) {
+//   List.findById(req.params.id).then(models => {
+//     res.json(models);
+//   });
+// });
+
+app.post("/models", function(req, res) {
+  Country.create(req.body).then(Country => {
+    res.json(list);
+  });
+});
+
+app.post("/list/:id/item", function(req, res) {
+  Country.findByIdAndUpdate(
+    req.params.id,
+    { $push: { items: req.body } },
+    { new: true }
+  ).then(Country => {
+    res.json(Country);
+  });
+});
+
+app.listen(3000, () =>
+  console.log("Is your server running? Better go catch it!")
+);
